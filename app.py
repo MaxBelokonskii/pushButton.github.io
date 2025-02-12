e6b98c1258e8e35e1abeb09d4a26c231a3d03bb6
maximBelokonskiy <m.belokonskiy@trucker.group>
2025-02-12 10:34:03 +0300

    add app settings

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ def handle_button_click(data):
             emit('update_log', {'log': f"{user_name} - Таймер еще не запущен."}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=False,  allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False,  allow_unsafe_werkzeug=True)
